bc2e15bdb6e682a3ab8f84c678ce0ffdff5240b3
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-02-11 00:20:53 +0530

    Added most functionality

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
 import praw
 from config import bot_username, bot_password
+
 r = praw.Reddit(user_agent='Mind The Gap Bot')
-r.login('bot_username', 'bot_password')
-submissions = r.get_subreddit('soccer').get_hot(limit=5)
-for x in submissions:
-	print str(x)
+
+r.login(bot_username, bot_password)
+
+submissions = r.get_subreddit('test').get_hot(limit=5)
+
+reply_text='Mind that gap '
+
+for submission in submissions:
+    flat_comments = praw.helpers.flatten_tree(submission.comments)
+    for comment in flat_comments:
+        if "Spurs" in comment.body:
+            comment.reply(reply_text)
