1bb6a6b3df852b016c32ea71a425a367dca20991
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-02-11 13:08:42 +0530

    Added comment

--- bot.py
+++ bot.py
@@ -4,13 +4,19 @@ from config import bot_username, bot_password
 r = praw.Reddit(user_agent='Mind The Gap Bot')
 
 r.login(bot_username, bot_password)
+print "Logged in"
 
-submissions = r.get_subreddit('test').get_hot(limit=5)
+submissions = r.get_subreddit('soccer').get_hot(limit=100)
+print "Got posts"
 
-reply_text='Mind that gap '
+reply_text='Mind that gap http://i.imgur.com/8uEKnyu.png'
 
 for submission in submissions:
     flat_comments = praw.helpers.flatten_tree(submission.comments)
     for comment in flat_comments:
-        if "Spurs" in comment.body:
-            comment.reply(reply_text)
+        try:
+            if "Spurs" in comment.body:
+                print comment.body
+                comment.reply(reply_text)
+        except:
+            pass
